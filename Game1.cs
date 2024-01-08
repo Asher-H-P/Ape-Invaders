e8f6fde7cb3ca7112dc6b369552dd9ca6ab2426b
Asher Heywood Peters <339513541@GoTVDSB.ca>
2024-01-08 11:02:16 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Ape_Invaders
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D A;
+        Texture2D B;
+        Texture2D C;
+        Texture2D D;
+        Texture2D E;
+        Texture2D F;
+        Texture2D G;
+        Texture2D H;
+        Texture2D I;
+        Texture2D J;
+        Texture2D K;
+        Texture2D L;
+        Texture2D M;
+        Texture2D N;
+        Texture2D O;
+        Texture2D P;
+        Texture2D Q;
+        Texture2D R;
+        Texture2D S;
+        Texture2D T;
+        Texture2D U;
+        Texture2D V;
+        Texture2D W;
+        Texture2D X;
+        Texture2D Y;
+        Texture2D Z;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1000;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            A = Content.Load<Texture2D>("AE-A");
+            B = Content.Load<Texture2D>("AE-B");
+            C = Content.Load<Texture2D>("AE-C");
+            D = Content.Load<Texture2D>("AE-D");
+            E = Content.Load<Texture2D>("AE-E");
+            F = Content.Load<Texture2D>("AE-F");
+            G = Content.Load<Texture2D>("AE-G");
+            H = Content.Load<Texture2D>("AE-H");
+            I = Content.Load<Texture2D>("AE-I");
+            J = Content.Load<Texture2D>("AE-J");
+            K = Content.Load<Texture2D>("AE-K");
+            L = Content.Load<Texture2D>("AE-L");
+            M = Content.Load<Texture2D>("AE-M");
+            N = Content.Load<Texture2D>("AE-N");
+            O = Content.Load<Texture2D>("AE-O");
+            P = Content.Load<Texture2D>("AE-P");
+            Q = Content.Load<Texture2D>("AE-Q");
+            R = Content.Load<Texture2D>("AE-R");
+            S = Content.Load<Texture2D>("AE-S");
+            T = Content.Load<Texture2D>("AE-T");
+            U = Content.Load<Texture2D>("AE-U");
+            V = Content.Load<Texture2D>("AE-V");
+            W = Content.Load<Texture2D>("AE-W");
+            X = Content.Load<Texture2D>("AE-X");
+            Y = Content.Load<Texture2D>("AE-Y");
+            Z = Content.Load<Texture2D>("AE-Z");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(A, new Vector2(300, 100), Color.White);
+            _spriteBatch.Draw(P, new Vector2(350, 100), Color.White);
+            _spriteBatch.Draw(E, new Vector2(400, 100), Color.White);
+            _spriteBatch.Draw(I, new Vector2(200, 200), Color.White);
+            _spriteBatch.Draw(N, new Vector2(250, 200), Color.White);
+            _spriteBatch.Draw(V, new Vector2(300, 200), Color.White);
+            _spriteBatch.Draw(A, new Vector2(350, 200), Color.White);
+            _spriteBatch.Draw(D, new Vector2(400, 200), Color.White);
+            _spriteBatch.Draw(E, new Vector2(450, 200), Color.White);
+            _spriteBatch.Draw(R, new Vector2(500, 200), Color.White);
+            _spriteBatch.Draw(S, new Vector2(550, 200), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
